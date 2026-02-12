65dbab7234d8149eee2969d14c55db702d2aa978
fishhow <fishhow.g@gmail.com>
2026-02-12 10:40:13 +0800

    【重构】对蓝牙功能重构

--- Arduino_code/epee_esp32_s3_v2/epee_esp32_s3_v2.ino
+++ Arduino_code/epee_esp32_s3_v2/epee_esp32_s3_v2.ino
@@ -6,9 +6,6 @@
 // LED 与板载指示灯
 const int LED_BOARD = 8;
 
-// --- 串口互斥锁（保留）---
-SemaphoreHandle_t serialMutex;
-
 // =====================【多核任务函数（原有逻辑未改动）】=====================
 void TaskLogic(void* pvParameters) {
   LogUtils::println("[核心1] 逻辑任务已启动");
@@ -39,16 +36,24 @@ void setup() {
   LogUtils::println("    重剑计分系统 S3 (带计时+遥控) 启动...");
   LogUtils::println("==============================");
 
-  serialMutex = xSemaphoreCreateMutex();
-  
-  FencingCore::getInstance()->init();
-  led_init();
-  led_on_boot();
-  pinMode(LED_BOARD, OUTPUT);
-
+  LogUtils::println("[主程序]完成蓝牙初始化");
   BluetoothManager::getInstance()->init();
+  
+  //蓝牙处理线程启动
   BluetoothManager::getInstance()->start();
 
+  LogUtils::println("[主程序]完成逻辑核心初始化");
+  FencingCore::getInstance()->init();
+
+  led_init();
+  led_on_boot();
+
+  pinMode(LED_BOARD, OUTPUT);
+
+  
+  
+
+  //逻辑处理线程
   xTaskCreatePinnedToCore(TaskLogic, "Logic", 8192, NULL, 2, NULL, 1);
 
   LogUtils::println("[系统] 所有任务已就绪");
